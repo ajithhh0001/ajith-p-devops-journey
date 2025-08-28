667c035862244915807f86facceacf773bd93869
ajithhh0001 <ajithhh0001@gmail.com>
2025-08-28 11:28:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/ajith-kumar-portfolio/' : '/',
+  base: mode === 'production' ? '/ajith-p-devops-journey/' : '/',
   server: {
     host: "::",
     port: 8080,
